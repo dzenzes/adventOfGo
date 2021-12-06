8f039f679248ceac7fb5759d26aa87d4b4a50444
Daniel Zenzes <daniel.zenzes@bike24.net>
2021-12-06 15:23:17 +0100

    day 03/21

--- 2021/day03/day03.go
+++ 2021/day03/day03.go
@@ -1,14 +1,26 @@
 package main
 
-import "strconv"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
 
-func GetMostCommonBit(input []string, position int) string {
-	count := 0
+	"github.com/dmies/adventOfGo/filehandler"
+)
+
+func CountOneInBinaries(input []string, position int) (count int) {
+
 	for _, line := range input {
 		if string(line[position]) == "1" {
 			count++
 		}
 	}
+	return
+}
+
+func GetMostCommonBit(input []string, position int) string {
+	count := CountOneInBinaries(input, position)
 	if count > len(input)-count {
 		return "1"
 	}
@@ -45,4 +57,75 @@ func CalculatePowerConsumption(input []string) (int, error) {
 		return 0, err
 	}
 	return gamma * epsilon, nil
+}
+
+func FilterByBit(input []string, bit string, position int) (result []string) {
+	for _, line := range input {
+		if string(line[position]) == bit {
+			result = append(result, line)
+		}
+	}
+	return
+}
+
+type filterFunction func(input []string, numberOfOnes int) string
+
+func CalculateRating(input []string, fn filterFunction) (int, error) {
+	for i := 0; i < len(input[0]) && len(input) > 1; i++ {
+		numberOfOnes := CountOneInBinaries(input, i)
+		filterBy := fn(input, numberOfOnes)
+		input = FilterByBit(input, filterBy, i)
+	}
+	if len(input) != 1 {
+		return 0, errors.New("couldn't find single binary")
+	}
+	oxygen, err := strconv.ParseInt(input[0], 2, 0)
+	if err != nil {
+		return 0, err
+	}
+	return int(oxygen), nil
+}
+
+func CalculateOxygenGeneratorRating(input []string) (int, error) {
+	return CalculateRating(input, func(input []string, numberOfOnes int) string {
+		filterBy := "0"
+		if numberOfOnes >= len(input)-numberOfOnes {
+			filterBy = "1"
+		}
+		return filterBy
+	})
+}
+
+func CalculateCO2ScrubberRating(input []string) (int, error) {
+	return CalculateRating(input, func(input []string, numberOfOnes int) string {
+		filterBy := "1"
+		if numberOfOnes >= len(input)-numberOfOnes {
+			filterBy = "0"
+		}
+		return filterBy
+	})
+}
+
+func CalculateLifeSupport(input []string) (int, error) {
+	oxygen, err := CalculateOxygenGeneratorRating(input)
+	if err != nil {
+		return 0, err
+	}
+	co2, err2 := CalculateCO2ScrubberRating(input)
+	if err2 != nil {
+		return 0, err2
+	}
+
+	return oxygen * co2, nil
+}
+
+func main() {
+	input, err := filehandler.ImportStringList("./input.txt")
+	if err != nil {
+		log.Fatalf("Couldn't read input")
+	}
+	power, _ := CalculatePowerConsumption(input)
+	fmt.Printf("day 03, part1 %v\n", power)
+	lifeSupport, _ := CalculateLifeSupport(input)
+	fmt.Printf("day 03, part2 %v\n", lifeSupport)
 }
--- 2021/day03/day03_test.go
+++ 2021/day03/day03_test.go
@@ -7,6 +7,43 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_CountOneInBinaries(t *testing.T) {
+
+	input := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
+	}
+
+	tests := []struct {
+		position int
+		want     int
+	}{
+		{0, 7},
+		{1, 5},
+		{2, 8},
+		{3, 7},
+		{4, 5},
+	}
+
+	for _, test := range tests {
+		t.Run("CountOneInBinaries at position "+fmt.Sprint(test.position), func(t *testing.T) {
+			got := CountOneInBinaries(input, test.position)
+			assert.Equal(t, test.want, got)
+		})
+
+	}
+}
+
 func Test_CalculateMostCommonBit(t *testing.T) {
 	input := []string{
 		"00100",
@@ -87,6 +124,131 @@ func Test_CalculatePowerConsumption(t *testing.T) {
 	}
 	want := 198
 	got, err := CalculatePowerConsumption(input)
+
+	if err != nil {
+		t.Errorf("couldn't calculate power consumption")
+	}
+	assert.Equal(t, want, got)
+}
+
+func Test_FilterByBit(t *testing.T) {
+
+	input := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
+	}
+
+	tests := []struct {
+		bit      string
+		position int
+		want     []string
+	}{
+		{
+			"0",
+			0,
+			[]string{
+				"00100",
+				"01111",
+				"00111",
+				"00010",
+				"01010",
+			},
+		},
+		{
+			"1",
+			1,
+			[]string{
+				"11110",
+				"01111",
+				"11100",
+				"11001",
+				"01010",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("FilterByBit("+fmt.Sprint(test.bit)+") at position "+fmt.Sprint(test.position), func(t *testing.T) {
+			got := FilterByBit(input, test.bit, test.position)
+			assert.Equal(t, test.want, got)
+		})
+
+	}
+}
+
+func Test_CalculateOxygenGeneratorRating(t *testing.T) {
+	input := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
+	}
+	want := 23
+	got, err := CalculateOxygenGeneratorRating(input)
+
+	if err != nil {
+		t.Errorf("couldn't calculate oxygen")
+	}
+	assert.Equal(t, want, got)
+}
+func Test_CalculateCO2ScrubberRating(t *testing.T) {
+	input := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
+	}
+	want := 10
+	got, err := CalculateCO2ScrubberRating(input)
+
+	if err != nil {
+		t.Errorf("couldn't calculate oxygen")
+	}
+	assert.Equal(t, want, got)
+}
+func Test_CalculateLifeSupport(t *testing.T) {
+	input := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
+	}
+	want := 230
+	got, err := CalculateLifeSupport(input)
 
 	if err != nil {
 		t.Errorf("couldn't calculate power consumption")
